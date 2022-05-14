23de35a0555007b9f7e7bb77cd47d13a049a24db
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-14 19:23:27 +0400

    added practis2

---
+++ practiss2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(master());
+        }
+          static bool master() 
+          {
+            Console.Write("birinci ededi daxil edin: ");
+            string astr = Console.ReadLine();
+            int a = int.Parse(astr);
+            bool result = false;
+
+            if (a % 21 ==0)
+            {
+                result = true;
+                return result;
+
+            }
+            return result;
+           
+          }
+    }
+}
